e2fdc91aa54b50d37d1132d1dc20f81382b84aa4
Dwi Siswanto <git@dw1.io>
2025-02-11 16:24:58 +0700

    test: enhance benchmark & add byte method tests
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- pcregexp_bench_test.go
+++ pcregexp_bench_test.go
@@ -8,82 +8,148 @@ import (
 )
 
 func BenchmarkCompile(b *testing.B) {
-	pattern := `\b\w+@\w+\.\w+\b`
+	patterns := []string{
+		`\b\w+@\w+\.\w+\b`,
+		`p([a-z]+)ch`,
+		`^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$`,
+		`(?<=foo)bar`,
+		`(\w+)\s+\1`,
+	}
 
-	b.Run("impl=pcregexp", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			re, _ := pcregexp.Compile(pattern)
-			re.Close()
-		}
-	})
+	for _, pattern := range patterns {
+		b.Run("pcregexp/"+pattern, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				re, _ := pcregexp.Compile(pattern)
+				re.Close()
+			}
+		})
 
-	b.Run("impl=stdlib", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			regexp.Compile(pattern)
-		}
-	})
+		b.Run("stdlib/"+pattern, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				regexp.Compile(pattern)
+			}
+		})
+	}
 }
 
 func BenchmarkMatchString(b *testing.B) {
-	pattern := `p([a-z]+)ch`
-	text := "peach punch pinch"
+	tests := []struct {
+		name    string
+		pattern string
+		text    string
+	}{
+		{"simple", `p([a-z]+)ch`, "peach punch pinch"},
+		{"email", `\b\w+@\w+\.\w+\b`, "test@example.com"},
+		// {"backreference", `(\w+)\s+\1`, "hello hello world"},
+		// {"lookaround", `(?<=foo)bar`, "foobar"},
+	}
 
-	pcre := pcregexp.MustCompile(pattern)
-	defer pcre.Close()
-	re := regexp.MustCompile(pattern)
+	for _, tt := range tests {
+		pcre := pcregexp.MustCompile(tt.pattern)
+		defer pcre.Close()
+		re := regexp.MustCompile(tt.pattern)
 
-	b.Run("impl=pcregexp", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			pcre.MatchString(text)
-		}
-	})
+		b.Run("pcregexp/"+tt.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				pcre.MatchString(tt.text)
+			}
+		})
 
-	b.Run("impl=stdlib", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			re.MatchString(text)
-		}
-	})
+		b.Run("stdlib/"+tt.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				re.MatchString(tt.text)
+			}
+		})
+	}
 }
 
-func BenchmarkFindString(b *testing.B) {
-	pattern := `p([a-z]+)ch`
-	text := "peach punch pinch"
+func BenchmarkFind(b *testing.B) {
+	tests := []struct {
+		name    string
+		pattern string
+		text    string
+	}{
+		{"simple", `p([a-z]+)ch`, "peach punch pinch"},
+		{"submatch", `(\w+)\s+(\w+)`, "hello world"},
+		{"no match", `xyz`, "abc def ghi"},
+	}
 
-	pcre := pcregexp.MustCompile(pattern)
-	defer pcre.Close()
-	re := regexp.MustCompile(pattern)
+	for _, tt := range tests {
+		pcre := pcregexp.MustCompile(tt.pattern)
+		defer pcre.Close()
+		re := regexp.MustCompile(tt.pattern)
 
-	b.Run("impl=pcregexp", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			pcre.FindString(text)
-		}
-	})
+		b.Run("pcregexp/Find/"+tt.name, func(b *testing.B) {
+			data := []byte(tt.text)
+			for i := 0; i < b.N; i++ {
+				pcre.Find(data)
+			}
+		})
 
-	b.Run("impl=stdlib", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			re.FindString(text)
-		}
-	})
+		b.Run("stdlib/Find/"+tt.name, func(b *testing.B) {
+			data := []byte(tt.text)
+			for i := 0; i < b.N; i++ {
+				re.Find(data)
+			}
+		})
+
+		b.Run("pcregexp/FindString/"+tt.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				pcre.FindString(tt.text)
+			}
+		})
+
+		b.Run("stdlib/FindString/"+tt.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				re.FindString(tt.text)
+			}
+		})
+	}
 }
 
-func BenchmarkReplaceAllString(b *testing.B) {
-	pattern := `p([a-z]+)ch`
-	text := "peach punch pinch"
-	repl := "FRUIT"
+func BenchmarkReplace(b *testing.B) {
+	tests := []struct {
+		name    string
+		pattern string
+		text    string
+		repl    string
+	}{
+		{"simple", `p([a-z]+)ch`, "peach punch pinch", "FRUIT"},
+		{"no match", `xyz`, "abc def ghi", "NONE"},
+		{"multiple", `\b\w+\b`, "one two three", "word"},
+	}
 
-	pcre := pcregexp.MustCompile(pattern)
-	defer pcre.Close()
-	re := regexp.MustCompile(pattern)
+	for _, tt := range tests {
+		pcre := pcregexp.MustCompile(tt.pattern)
+		defer pcre.Close()
+		re := regexp.MustCompile(tt.pattern)
 
-	b.Run("impl=pcregexp", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			pcre.ReplaceAllString(text, repl)
-		}
-	})
+		b.Run("pcregexp/ReplaceAll/"+tt.name, func(b *testing.B) {
+			src := []byte(tt.text)
+			repl := []byte(tt.repl)
+			for i := 0; i < b.N; i++ {
+				pcre.ReplaceAll(src, repl)
+			}
+		})
 
-	b.Run("impl=stdlib", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			re.ReplaceAllString(text, repl)
-		}
-	})
+		b.Run("stdlib/ReplaceAll/"+tt.name, func(b *testing.B) {
+			src := []byte(tt.text)
+			repl := []byte(tt.repl)
+			for i := 0; i < b.N; i++ {
+				re.ReplaceAll(src, repl)
+			}
+		})
+
+		b.Run("pcregexp/ReplaceAllString/"+tt.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				pcre.ReplaceAllString(tt.text, tt.repl)
+			}
+		})
+
+		b.Run("stdlib/ReplaceAllString/"+tt.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				re.ReplaceAllString(tt.text, tt.repl)
+			}
+		})
+	}
 }
--- pcregexp_test.go
+++ pcregexp_test.go
@@ -1,6 +1,8 @@
 package pcregexp_test
 
 import (
+	"bytes"
+	"reflect"
 	"testing"
 
 	"github.com/dwisiswant0/pcregexp"
@@ -176,4 +178,183 @@ func TestBackreferences(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestRegexp_ByteMethods(t *testing.T) {
+	re := pcregexp.MustCompile(`p([a-z]+)ch`)
+	defer re.Close()
+
+	t.Run("Match", func(t *testing.T) {
+		tests := []struct {
+			input []byte
+			want  bool
+		}{
+			{[]byte("peach"), true},
+			{[]byte("punch"), true},
+			{[]byte("pch"), false},
+		}
+
+		for _, tt := range tests {
+			if got := re.Match(tt.input); got != tt.want {
+				t.Errorf("Match(%q) = %v, want %v", tt.input, got, tt.want)
+			}
+		}
+	})
+
+	t.Run("Find", func(t *testing.T) {
+		tests := []struct {
+			input []byte
+			want  []byte
+		}{
+			{[]byte("peach punch"), []byte("peach")},
+			{[]byte("no match"), nil},
+		}
+
+		for _, tt := range tests {
+			got := re.Find(tt.input)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Find(%q) = %q, want %q", tt.input, got, tt.want)
+			}
+		}
+	})
+
+	t.Run("FindSubmatch", func(t *testing.T) {
+		input := []byte("peach")
+		want := [][]byte{[]byte("peach"), []byte("ea")}
+		got := re.FindSubmatch(input)
+
+		if len(got) != len(want) {
+			t.Errorf("FindSubmatch(%q) returned %d submatches, want %d", input, len(got), len(want))
+			return
+		}
+
+		for i := range got {
+			if !bytes.Equal(got[i], want[i]) {
+				t.Errorf("FindSubmatch(%q)[%d] = %q, want %q", input, i, got[i], want[i])
+			}
+		}
+	})
+}
+
+func TestRegexp_FindAll(t *testing.T) {
+	re := pcregexp.MustCompile(`p([a-z]+)ch`)
+	defer re.Close()
+
+	t.Run("FindAllString", func(t *testing.T) {
+		tests := []struct {
+			input string
+			n     int
+			want  []string
+		}{
+			{"peach punch pinch", -1, []string{"peach", "punch", "pinch"}},
+			{"peach punch pinch", 2, []string{"peach", "punch"}},
+			{"peach punch pinch", 0, nil},
+			{"no matches", -1, nil},
+		}
+
+		for _, tt := range tests {
+			got := re.FindAllString(tt.input, tt.n)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FindAllString(%q, %d) = %v, want %v", tt.input, tt.n, got, tt.want)
+			}
+		}
+	})
+
+	t.Run("FindAllStringSubmatch", func(t *testing.T) {
+		input := "peach punch"
+		want := [][]string{
+			{"peach", "ea"},
+			{"punch", "un"},
+		}
+		got := re.FindAllStringSubmatch(input, -1)
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("FindAllStringSubmatch(%q, -1) = %v, want %v", input, got, want)
+		}
+	})
+
+	t.Run("FindAllStringIndex", func(t *testing.T) {
+		input := "peach punch"
+		want := [][]int{{0, 5}, {6, 11}}
+		got := re.FindAllStringIndex(input, -1)
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("FindAllStringIndex(%q, -1) = %v, want %v", input, got, want)
+		}
+	})
+}
+
+func TestRegexp_ReplaceAll(t *testing.T) {
+	re := pcregexp.MustCompile(`a([a-z])e`)
+	defer re.Close()
+
+	t.Run("ReplaceAll", func(t *testing.T) {
+		tests := []struct {
+			src  []byte
+			repl []byte
+			want []byte
+		}{
+			{[]byte("age ace"), []byte("X"), []byte("X X")},
+			{[]byte("no match"), []byte("X"), []byte("no match")},
+		}
+
+		for _, tt := range tests {
+			got := re.ReplaceAll(tt.src, tt.repl)
+			if !bytes.Equal(got, tt.want) {
+				t.Errorf("ReplaceAll(%q, %q) = %q, want %q", tt.src, tt.repl, got, tt.want)
+			}
+		}
+	})
+
+	t.Run("ReplaceAllFunc", func(t *testing.T) {
+		input := []byte("age ace")
+		want := []byte("AGE ACE")
+		got := re.ReplaceAllFunc(input, bytes.ToUpper)
+
+		if !bytes.Equal(got, want) {
+			t.Errorf("ReplaceAllFunc(%q, bytes.ToUpper) = %q, want %q", input, got, want)
+		}
+	})
+}
+
+func TestRegexp_Split(t *testing.T) {
+	re := pcregexp.MustCompile(`\s+`)
+	defer re.Close()
+
+	tests := []struct {
+		input string
+		n     int
+		want  []string
+	}{
+		{"foo bar baz", -1, []string{"foo", "bar", "baz"}},
+		{"foo bar baz", 2, []string{"foo", "bar baz"}},
+		{"foo", -1, []string{"foo"}},
+		{"", -1, []string{""}},
+	}
+
+	for _, tt := range tests {
+		got := re.Split(tt.input, tt.n)
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("Split(%q, %d) = %v, want %v", tt.input, tt.n, got, tt.want)
+		}
+	}
+}
+
+func TestRegexp_Utility(t *testing.T) {
+	pattern := `p([a-z]+)ch`
+	re := pcregexp.MustCompile(pattern)
+	defer re.Close()
+
+	t.Run("String", func(t *testing.T) {
+		if got := re.String(); got != pattern {
+			t.Errorf("String() = %q, want %q", got, pattern)
+		}
+	})
+
+	// t.Run("NumSubexp", func(t *testing.T) {
+	// 	want := 1
+	// 	if got := re.NumSubexp(); got != want {
+	// 		t.Errorf("NumSubexp() = %d, want %d", got, want)
+	// 	}
+	// })
 }
